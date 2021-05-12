bbee63562b925bd353a52e3ea01c58761529e82a
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2021-05-12 14:47:43 -0300

    feat: avaliador Deve buscar os 3 meiores valores

--- tests/Services/AvaliadorTest.php
+++ tests/Services/AvaliadorTest.php
@@ -79,4 +79,28 @@ class AvaliadorTest extends TestCase
 
     self::assertEquals(2000, $menorValor);
   }
+
+  public function testAvalidadorDeveBuscar3MaioresValores()
+  {
+    $leilao = new Leilao('Fiat 147 0km');
+
+    $maria = new Usuario('Maria');
+    $joao = new Usuario('Joao');
+    $ana = new Usuario('Ana');
+    $jorge = new Usuario('Jorge');
+
+    $leilao->recebeLance(new Lance($joao, 2000));
+    $leilao->recebeLance(new Lance($ana, 1000));
+    $leilao->recebeLance(new Lance($maria, 2500));
+    $leilao->recebeLance(new Lance($jorge, 1500));
+
+    $leiloreiro = new Avaliador();
+    $leiloreiro->avalia($leilao);
+
+    $maioresLances = $leiloreiro->getMaioresLances();
+    static::assertCount(3, $maioresLances);
+    static::assertEquals(2500, $maioresLances[0]->getValor());
+    static::assertEquals(2000, $maioresLances[1]->getValor());
+    static::assertEquals(1500, $maioresLances[2]->getValor());
+  }
 }
