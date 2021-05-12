e744c7fdd8a7e99e0bb2232931aee4a0964f3893
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2021-05-12 14:02:45 -0300

    feat: primeiro teste com phpUnit

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Versões usadas no projeto
+- PHP => 7.3.28
+- Composer => 2.0.13
+
+# Comando para baixar os pacotes
+```cmd
+composer dump
+```
+
+# Comando utilizado para instalar phpUnit
+```cmd
+composer require --dev phpunit/phpunit ^7
+```
+
+# Comando para verificar versão do phpUnit
+```cmd
+vendor\bin\phpunit --version
+```
+
+# Rodar o phpUnit
+```cmd
+vendor\bin\phpunit --colors tests
+```
---
+++ tests/Services/AvaliadorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Alura\Leilao\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Services\Avaliador;
+
+require 'vendor/autoload.php';
+
+class AvaliadorTest extends TestCase
+{
+  public function testAvaliadorDeveEncontrarOMaiorValorDeLanceEmOrdemCrescente() {
+    $leilao = new Leilao('Fiat 147 0km');
+
+    $maria = new Usuario('Maria');
+    $joao = new Usuario('Joao');
+
+    $leilao->recebeLance(new Lance($joao, 2000));
+    $leilao->recebeLance(new Lance($maria, 2500));
+
+    $leiloreiro = new Avaliador();
+    $leiloreiro->avalia($leilao);
+
+    $maiorValor = $leiloreiro->getMaiorValor();
+
+    self::assertEquals(2500, $maiorValor);
+  }
+
+  public function testAvaliadorDeveEncontrarOMaiorValorDeLanceEmOrdemDescrescente() {
+    $leilao = new Leilao('Fiat 147 0km');
+
+    $maria = new Usuario('Maria');
+    $joao = new Usuario('Joao');
+
+    $leilao->recebeLance(new Lance($maria, 2500));
+    $leilao->recebeLance(new Lance($joao, 2000));
+
+    $leiloreiro = new Avaliador();
+    $leiloreiro->avalia($leilao);
+
+    $maiorValor = $leiloreiro->getMaiorValor();
+
+    self::assertEquals(2500, $maiorValor);
+  }
+}
