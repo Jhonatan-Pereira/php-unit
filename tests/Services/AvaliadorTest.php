06b2ca3676b0fc4cce2c972f6f2ef858ccb8145b
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2021-05-12 14:12:16 -0300

    feat: avaliador Deve encontrar o menor valor dos lances

--- tests/Services/AvaliadorTest.php
+++ tests/Services/AvaliadorTest.php
@@ -45,4 +45,38 @@ class AvaliadorTest extends TestCase
 
     self::assertEquals(2500, $maiorValor);
   }
+
+  public function testAvaliadorDeveEncontrarOMenorValorDeLanceEmOrdemDescrescente() {
+    $leilao = new Leilao('Fiat 147 0km');
+
+    $maria = new Usuario('Maria');
+    $joao = new Usuario('Joao');
+
+    $leilao->recebeLance(new Lance($maria, 2500));
+    $leilao->recebeLance(new Lance($joao, 2000));
+
+    $leiloreiro = new Avaliador();
+    $leiloreiro->avalia($leilao);
+
+    $menorValor = $leiloreiro->getMenorValor();
+
+    self::assertEquals(2000, $menorValor);
+  }
+
+  public function testAvaliadorDeveEncontrarOMenorValorDeLanceEmOrdemCrescente() {
+    $leilao = new Leilao('Fiat 147 0km');
+
+    $maria = new Usuario('Maria');
+    $joao = new Usuario('Joao');
+
+    $leilao->recebeLance(new Lance($joao, 2000));
+    $leilao->recebeLance(new Lance($maria, 2500));
+
+    $leiloreiro = new Avaliador();
+    $leiloreiro->avalia($leilao);
+
+    $menorValor = $leiloreiro->getMenorValor();
+
+    self::assertEquals(2000, $menorValor);
+  }
 }
